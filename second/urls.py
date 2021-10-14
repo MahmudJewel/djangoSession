2d657135832fc0f831107148e6a789979978a8f8
jewel <jmbengal.mbstu@gmail.com>
2021-10-14 13:43:12 +0600

    session set, get and delete

---
+++ second/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from second import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('',views.home, name='home' ),
+    #path('signup', views.signup, name='signup'),
+    #path('signin', LoginView.as_view(template_name='signin.html'),name='signin'),
+    #path('signout', LogoutView.as_view(template_name='signout.html'),name='signout'),
+
+    path('setsession', views.setSession, name='setsession'),    
+    path('getsession', views.getSession, name='getsession'),
+    path('delsession', views.deleteSession, name='deletesession'),
+    
+]
---
+++ second/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+
+def setSession(request):
+	request.session['name'] = 'Jewel'
+	return render(request,'setSession.html')
+
+def getSession(request):
+	nm=request.session.get('name')
+	context={
+		'nm':nm,
+	}
+	return render(request,'getSession.html',context)
+
+def deleteSession(request):
+	nm=request.session.get('name')
+	del request.session['name']
+	context={
+		'nm':nm,
+	}
+	return render(request,'delSession.html',context)
